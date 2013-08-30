b3618eae454410e0e7457cb8f96fb62724be8391
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-08-30 13:35:00 -0700

    initial commit

---
+++ QTKit/QTKit.cpp
@@ -0,0 +1,13 @@
+// QTKit.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hi there");
+
+  std::cout << "This is a native C++ program." << std::endl;
+  return 0;
+}
